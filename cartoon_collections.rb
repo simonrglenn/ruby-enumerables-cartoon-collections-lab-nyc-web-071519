8c9f99d5b8907870baa041e14957d20c0e2f5655
Simon Glenn <simonrglenn@gmail.com>
2019-07-13 20:51:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,10 @@ def long_planeteer_calls(words)
   words.any? { |word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  words.any? { |string| }
+  
+  
 end
