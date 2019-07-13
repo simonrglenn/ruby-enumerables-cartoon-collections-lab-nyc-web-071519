281f0c42daa67e7395190d69e9f3d201b3f9b46b
Simon Glenn <simonrglenn@gmail.com>
2019-07-13 20:16:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index{ |number, name| puts "number" + "name"}
 end
 
 def summon_captain_planet# code an argument here
@@ -14,3 +14,4 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
